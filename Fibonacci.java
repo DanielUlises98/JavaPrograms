55b1e065c9ca6b6a0849cdbd26b31605e8991236
DanielUlises98 <mendoza-garcia091998@outlook.com>
2021-09-23 02:21:41 -0500

    Fibonacci

---
+++ Fibonacci.java
@@ -0,0 +1,17 @@
+public class Fibonacci {
+    
+    public void PrintFibonacci(int count){
+        int n0 = 0;
+        int n1 = 1;
+        
+        System.out.print(n0+","+n1);
+
+        int sum= 0;
+        for(int i=1;i<=count;i++){
+            sum = n0 + n1;
+            n0 = n1;
+            n1=sum;
+            System.out.print(","+sum);
+        }
+    }
+}
---
+++ main.java
@@ -0,0 +1,7 @@
+class QuickStart {
+    public static void main(String []args){
+       Fibonacci f = new Fibonacci();
+       int until = 10;
+       f.PrintFibonacci(until);
+    }
+}
